5c76f147922f8747a50c0673fdb698540ee7bcc6
yash-r04 <150778551+yash-r04@users.noreply.github.com>
2024-06-16 19:02:32 +0530

    Update main.py
    added host='0.0.0.0', port=port

--- main.py
+++ main.py
@@ -84,5 +84,8 @@ def logout():
         session.pop(key)
     return redirect(url_for('/'))
 
+port = int(os.environ.get('PORT', 5000))
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,host='0.0.0.0', port=port)
